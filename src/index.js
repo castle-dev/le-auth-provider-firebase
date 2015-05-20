4599557328d061b1d62da2235a4a957cbebb9349
Scott Lowe <scott@entercastle.com>
2015-05-20 16:09:36 -0400

    feat(AuthProvider): add basic authentication methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-module.exports = {};
+var q = require('q');
+var Firebase = require('firebase');
+
+var AuthProviderFirebase = function (url) {
+  var _ref = new Firebase(url);
+  this.create = function (email, password) {
+    var deferred = q.defer();
+    _ref.createUser({
+      email: email,
+      password: password
+    }, function (err, authData) {
+      if (err) { deferred.reject(err); }
+      else { deferred.resolve(authData.uid); }
+    });
+    return deferred.promise;
+  };
+  this.loginWithEmail = function (email, password) {
+    var deferred = q.defer();
+    _ref.authWithPassword({
+      email: email,
+      password: password
+    }, function (err, authData) {
+      if (err) { return deferred.reject(err); }
+      else { deferred.resolve(authData.uid); }
+    });
+    return deferred.promise;
+  };
+  this.loginWithToken = function (token) {
+    var deferred = q.defer();
+    _ref.authWithCustomToken(token, function (err) {
+      if (err) { return deferred.reject(err); }
+      else { deferred.resolve(); }
+    });
+    return deferred.promise;
+  };
+  this.isAuthenticated = function () {
+    var authData = _ref.getAuth();
+    return !!authData;
+  };
+  this.logout = function () {
+    _ref.unauth();
+  };
+  this.requestPasswordReset = function (email) {
+    var deferred = q.defer();
+    _ref.resetPassword({ email: email }, function (err) {
+      if (err) { deferred.reject(err); }
+      else { deferred.resolve(); }
+    });
+    return deferred.promise;
+  };
+  this.resetPassword = function (email, token, password) {
+    var deferred = q.defer();
+    _ref.changePassword({
+      email: email,
+      oldPassword: token,
+      newPassword: password
+    }, function(err) {
+      if (err) { deferred.reject(err); }
+      else { deferred.resolve(); }
+    });
+    return deferred.promise;
+  }
+};
+
+module.exports = AuthProviderFirebase;
